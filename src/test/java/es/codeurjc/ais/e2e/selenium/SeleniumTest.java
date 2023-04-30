34234ac17edabc940f7dfc3e0d3e7e83931224b9
JoaquinFN21 <120312771+JoaquinFN21@users.noreply.github.com>
2023-04-30 18:39:42 +0200

    Update SeleniumTest.java

--- src/test/java/es/codeurjc/ais/e2e/selenium/SeleniumTest.java
+++ src/test/java/es/codeurjc/ais/e2e/selenium/SeleniumTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
